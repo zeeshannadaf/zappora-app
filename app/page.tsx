698442d43970df3123a141e280d35c91caa2c84b
zeeshannadaf <zeeshannadaf77@gmail.com>
2025-06-19 15:18:06 +0530

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
-      <h1 className="text-4xl font-bold text-purple-600">🚀 Welcome to Zappora</h1>
-      <p className="mt-4 text-lg text-gray-700">From Prompt to Product in a Zap</p>
-    </main>
-  );
-}
